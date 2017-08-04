4e70ecce818075135dc89d14d68c807d10a5e3fa
Defn Nghiem <iam@defn.sh>
2017-08-03 17:41:21 -0700

    faster convergence

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,48 +4,49 @@ set -exfu
 umask 0022
 
 function main {
-  if ! test -d .git; then
-    ssh -o StrictHostKeyChecking=no git@github.com true 2>/dev/null || true
-  fi
-
   local loader='sudo env DEBIAN_FRONTEND=noninteractive'
   local nm_branch="v20170617"
   local nm_remote="gh"
   local url_remote="https://github.com/imma/ubuntu"
 
-  git reset --hard
-  rsync -ia .gitconfig.template .gitconfig
+  if [[ ! -d .git ]]; then
+    ssh -o StrictHostKeyChecking=no git@github.com true 2>/dev/null || true
 
-  git remote add "${nm_remote}" "${url_remote}" 2>/dev/null || true
-  git remote set-url "${nm_remote}" "${url_remote}"
-  git fetch "${nm_remote}"
-  git branch -D "${nm_remote}/$nm_branch" || true
-  git branch --set-upstream-to "${nm_remote}/$nm_branch"
-  git reset --hard "${nm_remote}/${nm_branch}"
-  git checkout "${nm_branch}" 
-  git submodule update --init || git submodule foreach 'git reset --hard; git clean -ffd'
-  git submodule update --init
+    mv .git2 .git
 
-  $loader apt-get install -y awscli
-  $loader dpkg --configure -a
-  $loader apt-get update
-  $loader apt-get install -y make python build-essential aptitude
-  $loader aptitude hold grub-legacy-ec2 docker-ce lxd
-  $loader apt-get upgrade -y
+    git reset --hard
+    rsync -ia .gitconfig.template .gitconfig
 
-  work/base/script/bootstrap
-  work/jq/script/bootstrap
-  work/block/script/cibuild
-  source work/block/script/profile ~
-  make cache
-  require
+    git remote add "${nm_remote}" "${url_remote}" 2>/dev/null || true
+    git remote set-url "${nm_remote}" "${url_remote}"
+    git fetch "${nm_remote}"
+    git branch -D "${nm_remote}/$nm_branch" || true
+    git branch --set-upstream-to "${nm_remote}/$nm_branch"
+    git reset --hard "${nm_remote}/${nm_branch}"
+    git checkout "${nm_branch}" 
+    git submodule update --init || git submodule foreach 'git reset --hard; git clean -ffd'
+    git submodule update --init
 
-  chmod 700 .gnupg
-  chmod 600 .ssh/config
+    $loader apt-get install -y awscli
+    $loader dpkg --configure -a
+    $loader apt-get update
+    $loader apt-get install -y make python build-essential aptitude
+    $loader aptitude hold grub-legacy-ec2 docker-ce lxd
+    $loader apt-get upgrade -y
+
+    work/base/script/bootstrap
+    work/jq/script/bootstrap
+    work/block/script/cibuild
+    source work/block/script/profile ~
+    make cache
+    require
+
+    chmod 700 .gnupg
+    chmod 600 .ssh/config
+  fi
 
   block sync
   block bootstrap
-
   sync
 }
 
@@ -64,7 +65,7 @@ ____EOF
     fi
 
     if ! [[ -d ~ubuntu/.git ]]; then
-      rsync -ia /tmp/home/.git/. ~ubuntu/.git/
+      rsync -ia /tmp/home/.git/. ~ubuntu/.git2/
       chown -R ubuntu:ubuntu ~ubuntu
     fi
 
