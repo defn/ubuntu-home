a507a6c7dd44e7366ce28da4a0631be07eb6f72d
Defn Nghiem <iam@defn.sh>
2017-11-12 17:00:53 -0800

    ignore nfs-common package errors

--- bootstrap-aws.sh
+++ bootstrap-aws.sh
@@ -45,7 +45,7 @@ function main {
   esac
 
   $loader mkdir -p /data
-  $loader apt-get install -y nfs-common
+  $loader apt-get install -y nfs-common || true
   $loader mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 efs.adm1.immanent.io:/ /data || true
 
   export LANG=en_US.UTF-8
