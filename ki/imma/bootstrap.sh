30aa0deb1a03ce4b856b22dc851302dffbbe3c65
Defn Nghiem <iam@defn.sh>
2018-01-21 00:02:30 -0800

    add that root thing

--- ki/imma/bootstrap.sh
+++ ki/imma/bootstrap.sh
@@ -7,12 +7,17 @@ function main {
   cd "$HOME"
   source .bash_profile
 
-  sudo chgrp docker /var/run/docker.sock
-
   ssh -o StrictHostKeyChecking=no git@github.com true 2>/dev/null || true
 
   bl sync
   bl bootstrap
 }
 
-main "$@"
+if [[ "$(id -u -n)" == "root" ]]; then
+  if [[ -r "$HOME/.ssh/ssh_auth_sock" ]]; then
+    export SSH_AUTH_SOCK="$HOME/.ssh/ssh_auth_sock"
+  fi
+  ssh -A -o BatchMode=yes -o StrictHostKeyChecking=no ubuntu@localhost "$0"
+else
+  main "$@"
+fi
