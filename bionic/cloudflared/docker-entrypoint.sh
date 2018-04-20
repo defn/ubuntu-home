5e5ba8c51c1e876400f171933534b1f54adbc91b
Amanibhavam <iam@defn.sh>
2018-04-19 21:40:34 -0700

    cloudflared

---
+++ bionic/cloudflared/docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -efu
+
+nm_program="${BASH_SOURCE##*/}"
+nm_program="${nm_program%-entrypoint.sh}"
+
+if [[ "${1:0:1}" = '-' ]]; then
+  set -- "$nm_program" "$@"
+fi
+
+set -x
+exec "$@"
