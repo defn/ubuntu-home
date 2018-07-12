b189719e506360136bcd0103b6b420347ea1705b
Amanibhavam <iam@defn.sh>
2018-07-12 09:47:22 -0700

    use require outside of ns

--- cljs/src/defn/core.cljs
+++ cljs/src/defn/core.cljs
@@ -1,7 +1,7 @@
-(ns defn.core
-  (:require
-   ["express" :as express]
-   ["http" :as http]))
+(ns defn.core)
+
+(require '["express" :as express])
+(require '["http" :as http])
 
 (def app (express))
 
@@ -11,5 +11,3 @@
 (defn -main [& args]
   (doto (.createServer http #(app %1 %2))
     (.listen 3000)))
-
-(set! *main-cli-fn* -main)
