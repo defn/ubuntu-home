88289c1d5a37cec1261ceb11716fba29e81271bf
Amanibhavam <iam@defn.sh>
2018-07-13 22:07:10 -0700

    going for reageant

--- cljs/src/immanent/app.cljs
+++ cljs/src/immanent/app.cljs
@@ -1,6 +1,14 @@
-(ns immanent.app)
+(ns immanent.app
+  (:require [reagent.core :as r]))
 
-(enable-console-print!)
+(defonce click-count (r/atom 0))
+
+(defn state-ful-with-atom []
+  [:div {:on-click #(swap! click-count inc)}
+   "I have been clicked " @click-count " times."])
 
 (defn -main []
-  (new js/Vue (clj->js { :el "#app" :data { :message "Hello Vue!" } })))
+  (r/render [state-ful-with-atom]
+            (.-body js/document)))
+
+(enable-console-print!)
