1d9a2f2d815e870cb918902da1f240b68992248b
Amanibhavam <iam@defn.sh>
2018-07-17 15:58:13 -0700

    simple express handler retrieving stuff from s3

--- cljs/src/immanent/cli.cljs
+++ cljs/src/immanent/cli.cljs
@@ -1,8 +1,16 @@
-(ns immanent.cli)
+(ns immanent.cli
+  (:require [cljs.pprint :as pp]))
 
 (def aws (js/require "aws-sdk"))
+(def express (js/require "express"))
+
+(defonce buckets {})
+
+(defonce app (express))
 
 (defn -main [& args]
-  (.listBuckets (new aws.S3 {:apiVersion "2006-03-01"}) (fn [err data] (prn data))))
+  (.get app "/" (fn [req res] (.send res (str buckets))))
+  (.listen app 3000 (fn [] (prn "listening on port 3000")))
+  (.listBuckets (new aws.S3 {:apiVersion "2006-03-01"}) (fn [err data] (set! buckets (js->clj data)))))
 
 (set! *main-cli-fn* -main)
