883d8a1c79a932820e0322f82fb6042a21243460
Amanibhavam <iam@defn.sh>
2018-07-16 21:03:01 -0700

    dont combine js npm with cljsjs, rely on js to require the modules

--- cljs/src/immanent/cli.cljs
+++ cljs/src/immanent/cli.cljs
@@ -1,9 +1,12 @@
 (ns immanent.cli)
 
+(def aws (js/require "aws-sdk"))
+
 (defn square [x]
   (* x x))
 
 (defn -main [& args]
-  (-> args first js/parseInt square prn))
+  (-> args first js/parseInt square prn)
+  (prn aws))
 
 (set! *main-cli-fn* -main)
